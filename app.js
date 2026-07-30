c5aaf03fe159f02c7bd5d9eb1ad9eabca70c38f8
juancamilohenao23 <juanhenaovasco23@gmail.com>
2026-07-30 14:17:11 -0500

    segunda clase

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 import express from 'express';
-import "dotenv/config" 
+import {ConfigDotenv} from "dotenv" 
+import BodyParser from "body-parser";
 
 const app = express();
 const port = process.env.port || 3000;
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", (_, res) => {
     res.send('Hola, estamos aprendiendo express con la ficha 3407184');
 });
 
+app.get("/productos", (req, res)=>{
+    res.send(`<h1> listado de productos</h1>
+    <ol>
+        <li>Televisor</li>
+        <li>Celular</li>
+        <li>Impresora</li>
+        </ol>`)
+})
+
+app.get("/productos/nombre", (req,res)=>{
+    producto = req.params.nombre
+    res.send(`El producto es ${producto}`)
+})
 app.listen(port, () => {
     console.log(`Servidor en funcionamiento en el puerto: ${port}`);
 });
